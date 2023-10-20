986adb09c987b7bf87265f18b43be60c27261b13
Harsh panwar <panwarharshwardhan@email.com>
2023-10-20 16:38:23 +0530

    add Error handling

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const Listing = require("./models/listing.js");
 const methodOverride = require("method-override");
 const listing = require("./models/listing.js");
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsyc.js");
+const ExpressErroe = require("./utils/ExpressError.js");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname,"views"));
@@ -27,10 +29,11 @@ async function main(){
 };
 
 // index app 
-app.get("/listings",async(req,res) => {
+app.get("/listings",wrapAsync(async(req,res) => {
     const allListings = await Listing.find({});
     res.render("listings/index.ejs",{allListings})
-});
+}) 
+);
 
 //create new listing root
 app.get("/listings/new",(req,res) => {
@@ -38,43 +41,66 @@ app.get("/listings/new",(req,res) => {
 });
 
 //Show root
-app.get("/listings/:id",async(req,res) => {
+app.get("/listings/:id",wrapAsync(async(req,res) => {
     let id = req.params.id;
     
     let listing = await Listing.findById(id);
    
     res.render("listings/show.ejs",{listing});
-});
+ })
+);
 
 // edit root
-app.get("/listings/:id/edit",async(req,res) =>  {
+app.get("/listings/:id/edit",wrapAsync(async(req,res) =>  {
     let id = req.params.id;
     let listing = await Listing.findById(id);
     res.render("listings/edit.ejs",{listing});
-});
+ })
+);
 
-// Add new listing in db
-app.post("/listings",async(req,res) => {
+// Add new listing in db / create root
+app.post("/listings",wrapAsync(async(req,res,next) => { 
+    if(!req.body.listing) {
+        throw new ExpressErroe(400, "send valid data for listing");
+    }
     let newlisting = Listing(req.body.listing);
     await newlisting.save();
-    res.redirect("/listings")
-});
+    res.redirect("/listings");
+ }) 
+);
 
 // update root
-app.put("/listings/:id",async(req,res) => {
+app.put("/listings/:id",wrapAsync(async(req,res) => {
     let id = req.params.id;
     await Listing.findByIdAndUpdate(id,{...req.body.listing});
     res.redirect(`/listings`);
-});
+ })
+);
 
 //delete root
-app.delete("/listings/:id",async(req,res) => {
+app.delete("/listings/:id",wrapAsync( async(req,res) => {
     let id = req.params.id;
     deleteListing = await Listing.findByIdAndDelete(id);
     console.log(deleteListing);
     res.redirect("/listings");
+ })
+);
+
+//
+app.all("*", (req, res, next) => {
+    next(new ExpressErroe(404, "Page Not Found"));
+})
+
+// middleware
+// app.use((err,req,res,next) => {
+//     res.send("some thing wrong " );   
+// });
+
+app.use((err,req,res,next) => {
+    let  {status=500, message} = err;
+    res.status(status).send(message);
 });
 
 app.listen(8080,() => {
     console.log("srever is working")
-});
+})
